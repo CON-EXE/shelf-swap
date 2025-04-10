2fb109f41bc87be5c6d1960295bf8e5be18f25c8
Connor Hall <connrhall204@gmail.com>
2025-04-10 15:32:37 -0500

    Finished home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,21 @@
 'use strict';
 
+const { log } = console;
+
 const clock = document.querySelector('.clock');
 const battLevel = document.querySelector('.batt-level');
+
+const home = document.querySelector('.home');
+const search = document.querySelector('.search');
+const profile = document.querySelector('.profile');
+const swaps = document.querySelector('.swaps');
+const settings = document.querySelector('.settings');
+const listing = document.querySelector('.listing');
+
+const navButtons = document.querySelectorAll('nav a');
+const mainSections = document.querySelectorAll('main section');
+
+let current = [];
 
 function setClock(time) {
     let hour = time.getHours().toString().padStart(2, '0');
@@ -21,4 +35,17 @@ window.addEventListener('load', () => {
 
 navigator.getBattery().then(function(battery) {
     battLevel.innerText = `${battery.level * 100}%`;
+});
+
+navButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const targetId = button.getAttribute('id');
+        
+        mainSections.forEach(div => {
+            div.classList.remove('active'); // Hide all divs
+        });
+
+        const targetDiv = document.getElementById(targetId);
+        targetDiv.classList.add('active'); // Show the selected div
+    });
 });
